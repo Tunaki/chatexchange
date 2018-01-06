41ef5821a3830343d3d60c39d3ada449339ddbf0
Tunaki <guillaume_boue@orange.fr>
2018-01-06 20:14:01 +0100

    Make getName public and add getBaseUrl
    Reason: users may want to build their own URLs to get info about an aspect not covered in chatexchange. getName is to be made public so that users can also easily obtain the main host from the chat host.

--- src/main/java/fr/tunaki/stackoverflow/chat/ChatHost.java
+++ src/main/java/fr/tunaki/stackoverflow/chat/ChatHost.java
@@ -11,14 +11,25 @@ public enum ChatHost {
 	STACK_EXCHANGE("stackexchange.com"),
 	META_STACK_EXCHANGE("meta.stackexchange.com");
 
-	private final String name;
+	private final String name, baseUrl;
 
 	private ChatHost(String name) {
 		this.name = name;
+		this.baseUrl = "https://chat." + name;
 	}
 
-	String getName() {
+	/**
+	 * @return The name of the chat server this host represents (example: <code>stackoverflow.com</code>).
+	 */
+	public String getName() {
 		return name;
+	}
+
+	/**
+	 * @return The base URL that points to this chat server (example: <code>https://chat.stackoverflow.com</code>).
+	 */
+	public String getBaseUrl() {
+		return baseUrl;
 	}
 
 }
